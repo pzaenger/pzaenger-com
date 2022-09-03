2cd78ea6b370ace8c68f5618ec57ba6189fae830
pzaenger <dev@pzaenger.com>
2022-09-03 23:58:05 +0200

    Added under construction state

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("dummy test", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/pzaenger.com/i);
   expect(linkElement).toBeInTheDocument();
 });
